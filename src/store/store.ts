f183fb9ae323cc43f625f9d0bb31814b5cc439b4
humbkr <humbkr@gmail.com>
2020-05-15 13:49:20 +0200

    Disable redux devtools in prod.

--- src/store/store.ts
+++ src/store/store.ts
@@ -35,6 +35,7 @@ const store = configureStore({
       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
     },
   }),
+  devTools: process.env.NODE_ENV !== 'production',
 })
 
 const persistor = persistStore(store)
